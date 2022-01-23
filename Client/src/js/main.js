42074095ac6ed234b448a15c6cf9bce2a3ce3569
Philipp Heck <philippmichaelheck@icloud.com>
2022-01-23 23:18:26 +0100

    Hab so ziemlich alles hinzugefügt, außer der QR-Code-Bestätigung nach dem Buchen und die Paginierung der Vorstellungen funktioniert nicht.

--- Client/src/js/main.js
+++ Client/src/js/main.js
@@ -1,13 +1,152 @@
-console.log('Client-side code running');
+const fetch = require('cross-fetch');
+const sessionstorage = require('sessionstorage');
 
-const betreiberButton = document.getElementById('betreiber');
+window.addEventListener('load', onLoad);
 
-betreiberButton.addEventListener('click', () => {
-  console.log('Willkommen Betreiber');
-});
+function onLoad () {
+  if (document.title === 'Kunde') {
+    fetchVorstellungen();
+  } else if (document.title === 'Vorstellung') {
+    // console.log(sessionstorage.getItem('vorstellung'));
+    fetchSitzplaetze(sessionstorage.getItem('vorstellung'));
+  }
+}
 
-const kundeButton = document.getElementById('kunde');
+function fetchVorstellungen () {
+  fetch('/getVorstellung', { method: 'GET' })
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error('Request failed.');
+    })
+    .then((data) => {
+      makeVorstellungList(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-kundeButton.addEventListener('click', () => {
-  console.log('Willkommen Kunde');
-});
+function fetchSitzplaetze (vorstellungString) {
+  const temp = vorstellungString.split('.');
+  const vorstellung = { _id: temp[0], date: temp[1], time: temp[2], saal: temp[3], name: temp[4] };
+  // console.log(vorstellung);
+  fetch('/getKinosaal', { method: 'GET' })
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error('Request failed.');
+    })
+    .then((kinosäle) => {
+      fetch('/getReservierung', { method: 'GET' })
+        .then((response) => {
+          if (response.ok) return response.json();
+          throw new Error('Request failed.');
+        })
+        .then((reservierungen) => {
+          makeSitzList(vorstellung, reservierungen, kinosäle);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function makeSitzList (vorstellung, reservierungen, kinosäle) {
+  let reservierteSitze = [];
+  for (let i = 0; i < reservierungen.length; i++) {
+    if (reservierungen[i].vorstellung === vorstellung.name) {
+      reservierteSitze.push(reservierungen[i].sitze);
+    }
+  }
+  reservierteSitze = reservierteSitze.flat();
+  // console.log(reservierteSitze);
+
+  let kinosaal;
+  for (let i = 0; i < kinosäle.length; i++) {
+    if (kinosäle[i].name === vorstellung.saal) {
+      kinosaal = kinosäle[i];
+    }
+  }
+
+  // console.log(kinosaal);
+
+  const sitzePerKinosaal = [];
+  for (let i = 1; i <= kinosaal.sitzreihen; i++) {
+    for (let j = 1; j <= kinosaal.sitze; j++) {
+      sitzePerKinosaal.push(i + '.' + j);
+    }
+  }
+
+  // console.log(sitzePerKinosaal);
+
+  const restSitze = sitzePerKinosaal;
+  for (let i = 0; i < reservierteSitze.length; i++) {
+    const index = restSitze.indexOf(reservierteSitze[i]);
+    if (index > -1) {
+      restSitze.splice(index, 1);
+    }
+  }
+
+  // console.log(restSitze);
+
+  const sitzContainer = document.getElementById('sitzContainer');
+
+  const vorstellungItem = document.createElement('input');
+  vorstellungItem.setAttribute('type', 'hidden');
+  vorstellungItem.setAttribute('name', 'vorstellung');
+  vorstellungItem.setAttribute('value', vorstellung.name);
+
+  sitzContainer.appendChild(vorstellungItem);
+
+  for (let i = 0; i < restSitze.length; i++) {
+    const listItem = document.createElement('input');
+    listItem.setAttribute('type', 'checkbox');
+    listItem.setAttribute('value', restSitze[i]);
+    listItem.setAttribute('id', restSitze[i]);
+    listItem.setAttribute('name', 'sitze');
+
+    sitzContainer.appendChild(listItem);
+
+    const label = document.createElement('label');
+    label.setAttribute('for', restSitze[i]);
+
+    const reiheSitze = restSitze[i].split('.');
+    label.innerHTML = 'Reihe ' + reiheSitze[0] + ', Sitz ' + reiheSitze[1] + '<br>';
+
+    sitzContainer.appendChild(label);
+  }
+
+  const button = document.createElement('button');
+  button.setAttribute('type', 'submit');
+  button.innerHTML = 'Reservieren';
+  sitzContainer.appendChild(button);
+}
+
+function makeVorstellungList (data) {
+  // console.log(data);
+  const listContainer = document.getElementById('listContainer');
+
+  for (let i = 0; i < data.length; i++) {
+    const listItem = document.createElement('fieldset');
+    listItem.setAttribute('id', data[i]._id);
+    listItem.setAttribute('class', 'listItem');
+    listItem.addEventListener('click', function () {
+      window.location.href = 'vorstellung.html';
+      sessionstorage.setItem('vorstellung', data[i]._id + '.' + data[i].date + '.' + data[i].time + '.' + data[i].saal + '.' + data[i].name);
+      fetchSitzplaetze(data[i]);
+    });
+    const legend = document.createElement('legend');
+    legend.innerHTML = 'Vorstellung';
+    const itemName = document.createElement('p');
+    itemName.innerHTML = 'Film: ' + data[i].name;
+    const itemRoom = document.createElement('p');
+    itemRoom.innerHTML = 'Saal: ' + data[i].saal;
+    const itemDate = document.createElement('p');
+    itemDate.innerHTML = 'Vorstellungszeit: Am ' + data[i].date + ' um ' + data[i].time;
+    listItem.append(legend, itemName, itemRoom, itemDate);
+
+    listContainer.appendChild(listItem);
+  }
+}
--- Server/src/js/server.js
+++ Server/src/js/server.js
@@ -1,11 +1,16 @@
-console.log('Server-side code running');
-
 const express = require('express');
 const path = require('browserify');
 
+const MongoClient = require('mongodb').MongoClient;
 const server = express();
 
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded({ extended: true }));
+
 server.use(express.static('build'));
+
+const url = 'mongodb://localhost';
+let db;
 
 let port = 8080;
 
@@ -25,10 +30,150 @@ for (let i = 0; i < process.argv.length; i++) {
   }
 }
 
-server.listen(port, () => {
-  console.log('listening on ' + port);
+MongoClient.connect(url, (err, client) => {
+  if (err) {
+    return console.log(err);
+  }
+  db = client.db('kino');
+
+  server.listen(port, () => {
+    console.log('listening on ' + port);
+  });
 });
 
 server.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, '/../../build/index.html'));
+});
+
+// Kinosaal
+
+server.post('/addKinosaal', (req, res) => {
+  const kinosaal = {
+    name: req.body.name,
+    sitzreihen: req.body.sitzreihen,
+    sitze: req.body.sitze
+  };
+  // console.log(kinosaal);
+
+  db.collection('kinosaal').insertOne(kinosaal, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect(req.get('referer'));
+  });
+});
+
+server.get('/getKinosaal', (req, res) => {
+  db.collection('kinosaal').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    res.send(result);
+  });
+});
+
+// Vorstellung
+
+server.post('/addVorstellung', (req, res) => {
+  const vorstellung = {
+    date: req.body.date,
+    time: req.body.time,
+    saal: req.body.saal,
+    name: req.body.name
+  };
+  // console.log(vorstellung);
+
+  db.collection('kinosaal').findOne({ name: { $eq: vorstellung.saal } })
+    .then((response) => {
+      // console.log(response);
+      vorstellung.saal = response._id;
+
+      // console.log(vorstellung);
+      db.collection('vorstellung').insertOne(vorstellung, (err, result) => {
+        if (err) {
+          return console.log(err);
+        }
+        res.redirect(req.get('referer'));
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+server.get('/getVorstellung', (req, res) => {
+  db.collection('vorstellung').find().toArray((err, result) => {
+    if (err) return console.log(err);
+
+    const promises = [];
+    for (let i = 0; i < result.length; i++) {
+      promises.push(db.collection('kinosaal').findOne({ _id: { $eq: result[i].saal } })
+        .then((response) => {
+          // console.log(response);
+          result[i].saal = response.name;
+
+          // console.log(result[i]);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      );
+    }
+    Promise.allSettled(promises).then(() => {
+      res.send(result);
+    });
+  });
+});
+
+// Reservierungen
+
+server.post('/addReservierung', (req, res) => {
+  const reservierung = {
+    vorstellung: req.body.vorstellung,
+    sitze: req.body.sitze,
+    name: req.body.name
+  };
+
+  console.log(reservierung);
+
+  db.collection('vorstellung').findOne({ name: { $eq: reservierung.vorstellung } })
+    .then((response) => {
+      console.log(response);
+      reservierung.vorstellung = response._id;
+
+      console.log(reservierung);
+
+      db.collection('reservierung').insertOne(reservierung, (err, result) => {
+        if (err) {
+          return console.log(err);
+        }
+        res.redirect(req.get('referer'));
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+server.get('/getReservierung', (req, res) => {
+  db.collection('reservierung').find().toArray((err, result) => {
+    if (err) return console.log(err);
+
+    const promises = [];
+    for (let i = 0; i < result.length; i++) {
+      promises.push(db.collection('vorstellung').findOne({ _id: { $eq: result[i].vorstellung } })
+        .then((response) => {
+          if (response !== null) {
+            console.log(response);
+            result[i].vorstellung = response.vorstellung.name;
+          }
+          console.log(result[i]);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      );
+    }
+    Promise.allSettled(promises).then(() => {
+      res.send(result);
+    });
+  });
 });
