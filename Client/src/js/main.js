a855cc450c93d56443a68e1704f1e1db37271dde
xvHayu <95188869+xvHayu@users.noreply.github.com>
2022-02-01 20:11:22 +0100

    Paginierung von Vorstellungen und Sitzplätzen
    -Paginierung
    -Cursor & Border wird angepasst wenn Cursor über einem Element hovert

--- Client/src/js/main.js
+++ Client/src/js/main.js
@@ -163,8 +163,6 @@ function makeSitzList (vorstellung, reservierungen, kinosaele) {
     }
   }
   reservierteSitze = reservierteSitze.flat();
-  console.log(reservierteSitze);
-  console.log(vorstellung);
 
   let kinosaal;
   for (let i = 0; i < kinosaele.length; i++) {
@@ -189,8 +187,6 @@ function makeSitzList (vorstellung, reservierungen, kinosaele) {
     }
   }
 
-  console.log(restSitze);
-
   const sitzContainer = document.getElementById('sitzContainer');
 
   const vorstellungItem = document.createElement('input');
@@ -198,16 +194,16 @@ function makeSitzList (vorstellung, reservierungen, kinosaele) {
   vorstellungItem.setAttribute('name', 'vorstellung');
   vorstellungItem.setAttribute('value', vorstellung.name);
 
-  sitzContainer.appendChild(vorstellungItem);
+  sitzContainer.parentNode.appendChild(vorstellungItem);
 
   for (let i = 0; i < restSitze.length; i++) {
-    const listItem = document.createElement('input');
-    listItem.setAttribute('type', 'checkbox');
-    listItem.setAttribute('value', restSitze[i]);
-    listItem.setAttribute('id', restSitze[i]);
-    listItem.setAttribute('name', 'sitze');
+    const listItem = document.createElement('div');
 
-    sitzContainer.appendChild(listItem);
+    const input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('value', restSitze[i]);
+    input.setAttribute('id', restSitze[i]);
+    input.setAttribute('name', 'sitze');
 
     const label = document.createElement('label');
     label.setAttribute('for', restSitze[i]);
@@ -215,19 +211,92 @@ function makeSitzList (vorstellung, reservierungen, kinosaele) {
     const reiheSitze = restSitze[i].split('.');
     label.innerHTML = 'Reihe ' + reiheSitze[0] + ', Sitz ' + reiheSitze[1] + '<br>';
 
-    sitzContainer.appendChild(label);
+    listItem.appendChild(input);
+    listItem.appendChild(label);
+    sitzContainer.appendChild(listItem);
   }
 
-  const button = document.createElement('button');
-  button.setAttribute('type', 'submit');
-  button.innerHTML = 'Reservieren';
-  sitzContainer.appendChild(document.createElement('br'));
-  sitzContainer.appendChild(button);
+  makeSitzePagination();
+  window.addEventListener('resize', makeSitzePagination);
+}
+
+function makeSitzePagination () {
+  const sitzContainer = document.getElementById('sitzContainer');
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  const availableSpace = windowHeight - 260; // beruecksichtigt das letzte listItem, das bei while sichtbar gemacht wird
+
+  sitzContainer.setAttribute('style', '');
+
+  // alle vorstellungen verstecken
+  sitzContainer.childNodes.forEach((child) => {
+    child.setAttribute('style', 'display: none;');
+  });
+
+  // vorstellungen solange wieder anzeigen, bis fenstergroesse ausgeschoepft ist
+  let itemsPerPage = 0;
+  do {
+    sitzContainer.childNodes[itemsPerPage].setAttribute('style', '');
+    itemsPerPage++;
+  }
+  while (sitzContainer.clientHeight < availableSpace && itemsPerPage < sitzContainer.childNodes.length);
+
+  sitzContainer.setAttribute('style', 'height: ' + sitzContainer.clientHeight + 'px;');
+
+  const totalPages = Math.ceil(sitzContainer.childNodes.length / itemsPerPage);
+  selectSitzePage(1, itemsPerPage, totalPages);
+}
+
+function selectSitzePage (page, itemsPerPage, totalPages) {
+  const sitzContainer = document.getElementById('sitzContainer');
+  const pagination = document.getElementById('pagination');
+  pagination.innerHTML = '';
+
+  if (page > 2) {
+    const first = document.createElement('li');
+    first.innerHTML = '&laquo;';
+    first.addEventListener('click', () => selectSitzePage(1, itemsPerPage, totalPages));
+    pagination.appendChild(first);
+  }
+
+  for (let i = Math.max(1, page - 1); i <= Math.min(page + 1, totalPages); i++) {
+    const pageSelection = document.createElement('li');
+    pageSelection.innerHTML = i;
+    pageSelection.addEventListener('click', () => selectSitzePage(i, itemsPerPage, totalPages));
+    if (i === page) pageSelection.classList.add('selectedPage');
+    pagination.appendChild(pageSelection);
+  }
+
+  if (page < totalPages - 1) {
+    const last = document.createElement('li');
+    last.innerHTML = '&raquo;';
+    last.addEventListener('click', () => selectSitzePage(totalPages, itemsPerPage, totalPages));
+    pagination.appendChild(last);
+  }
+
+  // alle vorstellungen verstecken
+  sitzContainer.childNodes.forEach((child) => {
+    child.setAttribute('style', 'display: none;');
+  });
+
+  const firstItemIndex = (page - 1) * itemsPerPage;
+  const lastItemIndex = firstItemIndex + itemsPerPage;
+  for (let i = firstItemIndex; i < Math.min(lastItemIndex, sitzContainer.childNodes.length); i++) {
+    sitzContainer.childNodes[i].setAttribute('style', '');
+  }
 }
 
 function makeVorstellungList (data) {
   const listContainer = document.getElementById('listContainer');
-  // const itemHeight = 160;
+
+  data.sort((a, b) => {
+    if (a.date < b.date) return -1;
+    else if (a.date > b.date) return 1;
+    else {
+      if (a.time < b.time) return -1;
+      else if (a.time > b.time) return 1;
+      else return 0;
+    }
+  });
 
   for (let i = 0; i < data.length; i++) {
     const listItem = document.createElement('fieldset');
@@ -240,6 +309,7 @@ function makeVorstellungList (data) {
     });
     const legend = document.createElement('legend');
     legend.innerHTML = 'Vorstellung';
+
     const itemTable = document.createElement('table');
     const itemTableRow1 = document.createElement('tr');
     const itemTableRow2 = document.createElement('tr');
@@ -266,6 +336,73 @@ function makeVorstellungList (data) {
     itemTable.append(itemTableRow1, itemTableRow2, itemTableRow3);
     listItem.append(legend, itemTable);
     listContainer.appendChild(listItem);
+  }
+  makeVorstellungPagination();
+  window.addEventListener('resize', makeVorstellungPagination);
+}
+
+function makeVorstellungPagination () {
+  const listContainer = document.getElementById('listContainer');
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  const availableSpace = windowHeight - 250; // beruecksichtigt das letzte listItem, das bei while sichtbar gemacht wird
+
+  listContainer.setAttribute('style', '');
+
+  // alle vorstellungen verstecken
+  listContainer.childNodes.forEach((child) => {
+    child.setAttribute('style', 'display: none;');
+  });
+
+  // vorstellungen solange wieder anzeigen, bis fenstergroesse ausgeschoepft ist
+  let itemsPerPage = 0;
+  do {
+    listContainer.childNodes[itemsPerPage].setAttribute('style', '');
+    itemsPerPage++;
+  }
+  while (listContainer.clientHeight < availableSpace && itemsPerPage < listContainer.childNodes.length);
+
+  listContainer.setAttribute('style', 'height: ' + listContainer.clientHeight + 'px;');
+
+  const totalPages = Math.ceil(listContainer.childNodes.length / itemsPerPage);
+  selectVorstellungPage(1, itemsPerPage, totalPages);
+}
+
+function selectVorstellungPage (page, itemsPerPage, totalPages) {
+  const listContainer = document.getElementById('listContainer');
+  const pagination = document.getElementById('pagination');
+  pagination.innerHTML = '';
+
+  if (page > 2) {
+    const first = document.createElement('li');
+    first.innerHTML = '&laquo;';
+    first.addEventListener('click', () => selectVorstellungPage(1, itemsPerPage, totalPages));
+    pagination.appendChild(first);
+  }
+
+  for (let i = Math.max(1, page - 1); i <= Math.min(page + 1, totalPages); i++) {
+    const pageSelection = document.createElement('li');
+    pageSelection.innerHTML = i;
+    pageSelection.addEventListener('click', () => selectVorstellungPage(i, itemsPerPage, totalPages));
+    if (i === page) pageSelection.classList.add('selectedPage');
+    pagination.appendChild(pageSelection);
+  }
+
+  if (page < totalPages - 1) {
+    const last = document.createElement('li');
+    last.innerHTML = '&raquo;';
+    last.addEventListener('click', () => selectVorstellungPage(totalPages, itemsPerPage, totalPages));
+    pagination.appendChild(last);
+  }
+
+  // alle vorstellungen verstecken
+  listContainer.childNodes.forEach((child) => {
+    child.setAttribute('style', 'display: none;');
+  });
+
+  const firstItemIndex = (page - 1) * itemsPerPage;
+  const lastItemIndex = firstItemIndex + itemsPerPage;
+  for (let i = firstItemIndex; i < Math.min(lastItemIndex, listContainer.childNodes.length); i++) {
+    listContainer.childNodes[i].setAttribute('style', '');
   }
 }
 
